f73486ee3561e32b49d283709bb0841f3ced7506
MatiasG19 <m.galusek@gmail.com>
2024-09-24 20:32:25 +0200

    Update site

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://matiasg19.de',
 	integrations: [mdx(), sitemap()],
 });
